fafb181a3da8a29c3c48bd0ce3173dbdbab2af4c
Akshat <akshat9950@gmail.com>
2024-01-20 23:44:38 +0530

    Day 2 (Array)

--- Arrays/Day2.cpp
+++ Arrays/Day2.cpp
@@ -6,16 +6,16 @@ using namespace std;
 class Solution {
 public:
     int missingNumber(std::vector<int>& array, int n) {
-        // Calculate the sum of first N natural numbers
+        
         int expectedSum = (n * (n + 1)) / 2;
 
-        // Calculate the sum of the given array
+        
         int actualSum = 0;
         for (int num : array) {
             actualSum += num;
         }
 
-        // The missing number is the difference between the expected sum and actual sum
+        
         int missingNumber = expectedSum - actualSum;
 
         return missingNumber;
